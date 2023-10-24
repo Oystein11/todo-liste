4a94d5d2bbeae79b6535ddaf132f482b5eb6d811
Øystein <oystein.koder@gmail.com>
2023-10-24 12:38:37 +0200

    changes

--- node.js
+++ node.js
@@ -1,18 +1,31 @@
-const client = new client ();
+// Autentisering og database
+import { Client, Databases, Account, ID } from "appwrite";
 
-client
-    .setEndpoint ("http://localhost/v1")
-    .setProject ("653787afe8820d84f01e")
-;
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('653787afe8820d84f01e');
+
+const databases = new Databases(client);
 
 const account = new Account (client);
 
-//register user
-account.create (ID.unique(), "me@example.com", "password", "John Doe")
-    .then(function (response) {
-        console.log (response);
-    }, function (error){
-        console.log (error);
-    });
+const wish = account.createEmailSession ("email@example.com", "password");
 
-    
+wish.then (function (response) {
+    console.log(response);
+}, function (error) {
+    console.log(error);
+});
+
+const promise = databases.createDocument(
+    '653787fb47692d4c16a4',
+    'auto-generated',
+    ID.unique(),
+    { "title": "Hamlet" }
+);
+
+promise.then(function (response) {
+    console.log(response);
+}, function (error) {
+    console.log(error);
+});
