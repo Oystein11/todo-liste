48e111d4d462f63fcb76b920436bc4add2cb2fb7
Øystein <oystein.koder@gmail.com>
2023-10-24 11:17:45 +0200

    modifisering og innlegging av appwrite

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 // HTML-linker
 const input_text = document.querySelector ("#input_text_ID");
+console.log (input_text);
 const submit_task = document.querySelector ("#submit_task_ID");
+console.log (submit_task);
 const task_list = document.querySelector ("#task_list_ID");
+console.log (task_list);
 const todo_form = document.querySelector ("#todo_form_ID");
+console.log (todo_form);
 
 //Funksjoner
 // Legger til tekst som listeelementer, en sletteknapp ved siden av, og lagrer den lokalt
 function add_task () {
     if (input_text.value) {
         const new_task = document.createElement ("li");
+        console.log (new_task);
         const delete_button = document.createElement ("button");
         delete_button.textContent = "🗑️";
+        console.log (delete_button);
 
         new_task.textContent = input_text.value;
         task_list.appendChild (new_task);
@@ -29,6 +35,11 @@ task_list.addEventListener ("click", function (event) {
     save_data ();
 });
 
+// Fjerner alle listeelementer fra listen
+function remove_all () {
+
+}
+
 // Forhindrer siden fra å refreshe
 todo_form.addEventListener ("submit", function (event) {
     event.preventDefault ();
@@ -45,3 +56,12 @@ function load_data () {
     task_list.innerHTML = localStorage.getItem ("task_list");
 }
 load_data();
+
+// Legger inn autentisering
+// Lag en bruker
+
+
+//Innlogging for bruker
+
+
+//Legger inn databasefunksjonalitet
